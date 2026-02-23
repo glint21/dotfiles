a4693ef82f1b399c4f55751a02219fd325504fc8
glint21 <153311778+glint21@users.noreply.github.com>
2026-02-23 22:18:46 +0000

    nvim and tmux

---
+++ nvim/init.lua
@@ -0,0 +1,174 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.swapfile = false
+vim.g.mapleader = " "
+
+
+vim.keymap.set('n', '<leader>o', ':update<CR>:source<CR>')
+
+vim.pack.add({
+    { src = "https://github.com/catppuccin/nvim.git",                   name = "catppuccin" },
+    { src = "https://github.com/neovim/nvim-lspconfig.git",             name = "lspconfig" },
+    { src = "https://github.com/williamboman/mason.nvim",               name = "mason" },
+    { src = "https://github.com/williamboman/mason-lspconfig.nvim.git", name = "mason-lspconfig" },
+    { src = "https://github.com/hrsh7th/nvim-cmp.git",                  name = "cmp" },
+    { src = "https://github.com/hrsh7th/cmp-nvim-lsp.git",              name = "cmp-lsp" },
+    { src = "https://github.com/L3MON4D3/LuaSnip.git",                  name = "luasnip" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim.git",     name = "telescope" },
+    { src = "https://github.com/nvim-lua/plenary.nvim.git",             name = "plenary" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter.git",   name = "treesitter" },
+    { src = "https://github.com/windwp/nvim-autopairs.git",             name = "autopairs" },
+
+})
+
+require("catppuccin").setup({
+    flavour = "auto", -- latte, frappe, macchiato, mocha
+    background = {    -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = false, -- disables setting the background color.
+    float = {
+        transparent = false,        -- enable transparent floating windows
+        solid = false,              -- use solid styling for floating windows, see |winborder|
+    },
+    show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+    term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+        enabled = false,            -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15,          -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = false,              -- Force no italic
+    no_bold = false,                -- Force no bold
+    no_underline = false,           -- Force no underline
+    styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" },    -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    },
+    lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+        virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
+        },
+        underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+            ok = { "underline" },
+        },
+        inlay_hints = {
+            background = true,
+        },
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    default_integrations = true,
+    auto_integrations = false,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+    },
+})
+vim.cmd.colorscheme "catppuccin"
+
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+vim.lsp.enable({ "lua_ls", "pyright", "rust_analyzer" }, {
+    canabilities = capabilities
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+        end
+
+        local opts = { buffer = args.buf }
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    end,
+})
+
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" }
+    })
+})
+
+require("nvim-autopairs").setup({})
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
+require("nvim-treesitter").setup {
+    install_dir = vim.fn.stdpath('data') .. '/site'
+}
+
+require("nvim-treesitter").install({ "lua", "python", "rust" })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "lua", "python", "rust" },
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "lua", "python", "rust" },
+    callback = function()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+})
